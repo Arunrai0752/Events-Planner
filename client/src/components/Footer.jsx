886a22466235afc589e71862adfea4b469ca9ff4
Arunrai0752 <realarunrai0752@gmail.com>
2025-06-20 00:15:22 +0530

    Componenets File Created

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <div>vmvreneggkner</div>
+    </>
+  );
+};
+
+export default Footer;
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <div>efflowkgogkfoerk</div>
+    </>
+  );
+};
+
+export default Navbar;
