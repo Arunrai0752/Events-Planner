6ba760d44bda6c470818fd857283f9e7d787e498
Arunrai0752 <realarunrai0752@gmail.com>
2025-06-21 12:02:57 +0530

    set picture

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Hero from "../components/HomeHero";
 import Journey from "../components/JourneyBegin";
 import JourneyBegin from "../components/JourneyBegin";
+import PhotoGallery from "../components/PhotoGallery";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <JourneyBegin />
+      <PhotoGallery/>
     </>
   );
 };
