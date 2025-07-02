cb9b5d0b8ec0c55fadd37ee538298f1cab755bcf
Arunrai0752 <realarunrai0752@gmail.com>
2025-07-02 16:37:34 +0530

    Mongo DB Connected

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 import express from "express";
 import authRouter from "./src/routes/authRouter.js";
 import dotenv from "dotenv";
+import connectDB from "./src/config/db.js";
 dotenv.config();
 
-
 const App = express();
-
+App.use(express.json());
 App.use("/auth", authRouter);
 
 App.get("/", (req, res) => {
@@ -14,9 +14,9 @@ App.get("/", (req, res) => {
 
 // const port = 2323;
 
-const port  = process.env.PORT || 2323;
+const port = process.env.PORT || 2323;
 
-
-App.listen(port, () => {
+App.listen(port,  () => {
   console.log(`http://localhost:${port}/register`);
+    connectDB();
 });
